d46eed703e13dcbbbfb1cfd7d72bf32157d37fef
Osama Darouzi <odarouzi12@gmail.com>
2024-08-26 17:57:27 +0300

    frmMainMenu V1.1

--- Forms/frmMainMenu.Designer.cs
+++ Forms/frmMainMenu.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMainMenu));
-            this.guna2Shapes1 = new Guna.UI2.WinForms.Guna2Shapes();
+            this.Separator = new Guna.UI2.WinForms.Guna2Shapes();
             this.btnManageClients = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.btnUsersActions = new Guna.UI2.WinForms.Guna2CircleButton();
             this.btnLoginRegistry = new Guna.UI2.WinForms.Guna2CircleButton();
-            this.btnUsersAction = new Guna.UI2.WinForms.Guna2CircleButton();
             this.btnManageUsers = new Guna.UI2.WinForms.Guna2CircleButton();
             this.btnClientsActions = new Guna.UI2.WinForms.Guna2CircleButton();
             this.btnClientsTransfers = new Guna.UI2.WinForms.Guna2CircleButton();
@@ -44,7 +44,7 @@
             // PnlHead
             // 
             this.PnlHead.MinimumSize = new System.Drawing.Size(0, 94);
-            this.PnlHead.Size = new System.Drawing.Size(1099, 94);
+            this.PnlHead.Size = new System.Drawing.Size(1000, 94);
             this.PnlHead.Controls.SetChildIndex(this.lblUsername, 0);
             this.PnlHead.Controls.SetChildIndex(this.pbStar, 0);
             this.PnlHead.Controls.SetChildIndex(this.pbProPic, 0);
@@ -59,37 +59,37 @@
             // 
             // pbProPic
             // 
-            this.pbProPic.Location = new System.Drawing.Point(436, 0);
+            this.pbProPic.Location = new System.Drawing.Point(387, 0);
             // 
             // lnkFullName
             // 
-            this.lnkFullName.Location = new System.Drawing.Point(537, 10);
+            this.lnkFullName.Location = new System.Drawing.Point(488, 10);
             // 
             // lblTime
             // 
-            this.lblTime.Location = new System.Drawing.Point(937, 8);
-            this.lblTime.Text = "5:56 AM";
+            this.lblTime.Location = new System.Drawing.Point(838, 8);
             // 
             // lblUsername
             // 
-            this.lblUsername.Location = new System.Drawing.Point(538, 42);
+            this.lblUsername.Location = new System.Drawing.Point(489, 42);
             // 
-            // guna2Shapes1
+            // Separator
             // 
-            this.guna2Shapes1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.guna2Shapes1.BorderColor = System.Drawing.Color.White;
-            this.guna2Shapes1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.guna2Shapes1.Location = new System.Drawing.Point(0, 335);
-            this.guna2Shapes1.Name = "guna2Shapes1";
-            this.guna2Shapes1.PolygonSkip = 1;
-            this.guna2Shapes1.Rotate = 0F;
-            this.guna2Shapes1.Shape = Guna.UI2.WinForms.Enums.ShapeType.Line;
-            this.guna2Shapes1.Size = new System.Drawing.Size(1116, 10);
-            this.guna2Shapes1.TabIndex = 12;
-            this.guna2Shapes1.Text = "guna2Shapes1";
-            this.guna2Shapes1.Zoom = 80;
+            this.Separator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Separator.BorderColor = System.Drawing.Color.White;
+            this.Separator.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Separator.LineEndCap = System.Drawing.Drawing2D.LineCap.Round;
+            this.Separator.LineStartCap = System.Drawing.Drawing2D.LineCap.Round;
+            this.Separator.LineThickness = 6;
+            this.Separator.Location = new System.Drawing.Point(63, 335);
+            this.Separator.Name = "Separator";
+            this.Separator.PolygonSkip = 1;
+            this.Separator.Rotate = 0F;
+            this.Separator.Shape = Guna.UI2.WinForms.Enums.ShapeType.Line;
+            this.Separator.Size = new System.Drawing.Size(888, 10);
+            this.Separator.TabIndex = 12;
+            this.Separator.Text = "guna2Shapes1";
+            this.Separator.Zoom = 100;
             // 
             // btnManageClients
             // 
@@ -106,20 +106,39 @@
             this.btnManageClients.Image = ((System.Drawing.Image)(resources.GetObject("btnManageClients.Image")));
             this.btnManageClients.ImageOffset = new System.Drawing.Point(0, 37);
             this.btnManageClients.ImageSize = new System.Drawing.Size(150, 150);
-            this.btnManageClients.Location = new System.Drawing.Point(113, 126);
+            this.btnManageClients.Location = new System.Drawing.Point(63, 120);
             this.btnManageClients.Name = "btnManageClients";
             this.btnManageClients.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.btnManageClients.Size = new System.Drawing.Size(200, 200);
             this.btnManageClients.TabIndex = 12;
             this.btnManageClients.Text = "Manage Clients";
             this.btnManageClients.TextOffset = new System.Drawing.Point(0, 20);
-            this.btnManageClients.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            this.btnManageClients.Click += new System.EventHandler(this.btnManageClients_Click);
+            // 
+            // btnUsersActions
+            // 
+            this.btnUsersActions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnUsersActions.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnUsersActions.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnUsersActions.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnUsersActions.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnUsersActions.FillColor = System.Drawing.Color.Transparent;
+            this.btnUsersActions.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsersActions.ForeColor = System.Drawing.Color.Black;
+            this.btnUsersActions.Image = global::Bank.Properties.Resources.user__3_;
+            this.btnUsersActions.ImageOffset = new System.Drawing.Point(0, 37);
+            this.btnUsersActions.ImageSize = new System.Drawing.Size(150, 150);
+            this.btnUsersActions.Location = new System.Drawing.Point(407, 363);
+            this.btnUsersActions.Name = "btnUsersActions";
+            this.btnUsersActions.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnUsersActions.Size = new System.Drawing.Size(200, 200);
+            this.btnUsersActions.TabIndex = 13;
+            this.btnUsersActions.Text = "Users Actions";
+            this.btnUsersActions.TextOffset = new System.Drawing.Point(0, 20);
+            this.btnUsersActions.Click += new System.EventHandler(this.btnLoginRegistry_Click);
             // 
             // btnLoginRegistry
             // 
-            this.btnLoginRegistry.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.btnLoginRegistry.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnLoginRegistry.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnLoginRegistry.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -127,45 +146,21 @@
             this.btnLoginRegistry.FillColor = System.Drawing.Color.Transparent;
             this.btnLoginRegistry.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLoginRegistry.ForeColor = System.Drawing.Color.Black;
-            this.btnLoginRegistry.Image = global::Bank.Properties.Resources.user__3_;
-            this.btnLoginRegistry.ImageOffset = new System.Drawing.Point(0, 37);
-            this.btnLoginRegistry.ImageSize = new System.Drawing.Size(150, 150);
-            this.btnLoginRegistry.Location = new System.Drawing.Point(457, 363);
+            this.btnLoginRegistry.Image = global::Bank.Properties.Resources._enter;
+            this.btnLoginRegistry.ImageOffset = new System.Drawing.Point(-10, 37);
+            this.btnLoginRegistry.ImageSize = new System.Drawing.Size(125, 125);
+            this.btnLoginRegistry.Location = new System.Drawing.Point(751, 363);
             this.btnLoginRegistry.Name = "btnLoginRegistry";
             this.btnLoginRegistry.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.btnLoginRegistry.Size = new System.Drawing.Size(200, 200);
-            this.btnLoginRegistry.TabIndex = 13;
-            this.btnLoginRegistry.Text = "Users Actions";
+            this.btnLoginRegistry.TabIndex = 14;
+            this.btnLoginRegistry.Text = "Login Registry";
             this.btnLoginRegistry.TextOffset = new System.Drawing.Point(0, 20);
-            // 
-            // btnUsersAction
-            // 
-            this.btnUsersAction.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnUsersAction.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btnUsersAction.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btnUsersAction.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btnUsersAction.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnUsersAction.FillColor = System.Drawing.Color.Transparent;
-            this.btnUsersAction.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUsersAction.ForeColor = System.Drawing.Color.Black;
-            this.btnUsersAction.Image = global::Bank.Properties.Resources._enter;
-            this.btnUsersAction.ImageOffset = new System.Drawing.Point(-10, 37);
-            this.btnUsersAction.ImageSize = new System.Drawing.Size(125, 125);
-            this.btnUsersAction.Location = new System.Drawing.Point(801, 363);
-            this.btnUsersAction.Name = "btnUsersAction";
-            this.btnUsersAction.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.btnUsersAction.Size = new System.Drawing.Size(200, 200);
-            this.btnUsersAction.TabIndex = 14;
-            this.btnUsersAction.Text = "Login Registry";
-            this.btnUsersAction.TextOffset = new System.Drawing.Point(0, 20);
+            this.btnLoginRegistry.Click += new System.EventHandler(this.btnUsersAction_Click);
             // 
             // btnManageUsers
             // 
-            this.btnManageUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnManageUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.btnManageUsers.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnManageUsers.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnManageUsers.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -176,13 +171,14 @@
             this.btnManageUsers.Image = global::Bank.Properties.Resources.user_management;
             this.btnManageUsers.ImageOffset = new System.Drawing.Point(0, 37);
             this.btnManageUsers.ImageSize = new System.Drawing.Size(150, 150);
-            this.btnManageUsers.Location = new System.Drawing.Point(113, 363);
+            this.btnManageUsers.Location = new System.Drawing.Point(63, 363);
             this.btnManageUsers.Name = "btnManageUsers";
             this.btnManageUsers.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.btnManageUsers.Size = new System.Drawing.Size(200, 200);
             this.btnManageUsers.TabIndex = 15;
             this.btnManageUsers.Text = "Manage Users";
             this.btnManageUsers.TextOffset = new System.Drawing.Point(0, 20);
+            this.btnManageUsers.Click += new System.EventHandler(this.btnManageUsers_Click);
             // 
             // btnClientsActions
             // 
@@ -199,14 +195,15 @@
             this.btnClientsActions.Image = ((System.Drawing.Image)(resources.GetObject("btnClientsActions.Image")));
             this.btnClientsActions.ImageOffset = new System.Drawing.Point(0, 30);
             this.btnClientsActions.ImageSize = new System.Drawing.Size(140, 140);
-            this.btnClientsActions.Location = new System.Drawing.Point(457, 126);
+            this.btnClientsActions.Location = new System.Drawing.Point(407, 120);
             this.btnClientsActions.Name = "btnClientsActions";
+            this.btnClientsActions.Padding = new System.Windows.Forms.Padding(25);
             this.btnClientsActions.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.btnClientsActions.Size = new System.Drawing.Size(200, 200);
             this.btnClientsActions.TabIndex = 16;
             this.btnClientsActions.Text = "Transactions History";
             this.btnClientsActions.TextOffset = new System.Drawing.Point(0, 20);
-            this.btnClientsActions.Click += new System.EventHandler(this.guna2CircleButton5_Click);
+            this.btnClientsActions.Click += new System.EventHandler(this.btnClientsActions_Click);
             // 
             // btnClientsTransfers
             // 
@@ -224,28 +221,29 @@
             this.btnClientsTransfers.Image = global::Bank.Properties.Resources.Transfer;
             this.btnClientsTransfers.ImageOffset = new System.Drawing.Point(0, 37);
             this.btnClientsTransfers.ImageSize = new System.Drawing.Size(125, 125);
-            this.btnClientsTransfers.Location = new System.Drawing.Point(801, 126);
+            this.btnClientsTransfers.Location = new System.Drawing.Point(751, 120);
             this.btnClientsTransfers.Name = "btnClientsTransfers";
+            this.btnClientsTransfers.Padding = new System.Windows.Forms.Padding(50);
             this.btnClientsTransfers.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.btnClientsTransfers.Size = new System.Drawing.Size(200, 200);
             this.btnClientsTransfers.TabIndex = 17;
             this.btnClientsTransfers.Text = "Transfer History";
             this.btnClientsTransfers.TextOffset = new System.Drawing.Point(0, 20);
-            this.btnClientsTransfers.Click += new System.EventHandler(this.guna2CircleButton6_Click);
+            this.btnClientsTransfers.Click += new System.EventHandler(this.btnClientsTransfers_Click);
             // 
             // frmMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1099, 600);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(245)))), ((int)(((byte)(235)))));
+            this.ClientSize = new System.Drawing.Size(1000, 600);
             this.Controls.Add(this.btnClientsTransfers);
             this.Controls.Add(this.btnClientsActions);
             this.Controls.Add(this.btnManageUsers);
-            this.Controls.Add(this.btnUsersAction);
             this.Controls.Add(this.btnLoginRegistry);
+            this.Controls.Add(this.btnUsersActions);
             this.Controls.Add(this.btnManageClients);
-            this.Controls.Add(this.guna2Shapes1);
+            this.Controls.Add(this.Separator);
             this.MinimizeButton = true;
             this.MinimumSize = new System.Drawing.Size(0, 94);
             this.Mode = MyUC.Forms.frmScreen.enMode.User;
@@ -255,10 +253,10 @@
             this.Text = "frmMainMenu";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMainMenu_FormClosed);
             this.Controls.SetChildIndex(this.PnlHead, 0);
-            this.Controls.SetChildIndex(this.guna2Shapes1, 0);
+            this.Controls.SetChildIndex(this.Separator, 0);
             this.Controls.SetChildIndex(this.btnManageClients, 0);
+            this.Controls.SetChildIndex(this.btnUsersActions, 0);
             this.Controls.SetChildIndex(this.btnLoginRegistry, 0);
-            this.Controls.SetChildIndex(this.btnUsersAction, 0);
             this.Controls.SetChildIndex(this.btnManageUsers, 0);
             this.Controls.SetChildIndex(this.btnClientsActions, 0);
             this.Controls.SetChildIndex(this.btnClientsTransfers, 0);
@@ -272,10 +270,10 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2Shapes guna2Shapes1;
+        private Guna.UI2.WinForms.Guna2Shapes Separator;
         private Guna.UI2.WinForms.Guna2CircleButton btnManageClients;
+        private Guna.UI2.WinForms.Guna2CircleButton btnUsersActions;
         private Guna.UI2.WinForms.Guna2CircleButton btnLoginRegistry;
-        private Guna.UI2.WinForms.Guna2CircleButton btnUsersAction;
         private Guna.UI2.WinForms.Guna2CircleButton btnManageUsers;
         private Guna.UI2.WinForms.Guna2CircleButton btnClientsActions;
         private Guna.UI2.WinForms.Guna2CircleButton btnClientsTransfers;
--- Forms/frmMainMenu.cs
+++ Forms/frmMainMenu.cs
@@ -4,9 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using ControlsUtil;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static BankBusinessLayer.AppGlobals;
@@ -36,17 +37,61 @@ namespace Bank.Forms
             Application.Exit();
         }
 
-        private void guna2CircleButton6_Click(object sender, EventArgs e)
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Size size = new Size(Size.Width / 5, Size.Width / 5);
+            int ImageSize = 3 * size.Width / 4;
+            int UpY = Separator.Location.Y - size.Width - 30;
+            int LowY = Separator.Location.Y + 30;
+            
+            btnManageClients.Size = size;
+            btnManageClients.Location = new Point(Separator.Left, UpY);
+            // btnManageClients.ImageSize = new Size(ImageSize, ImageSize);
+
+            btnClientsActions.Size = size;
+            btnClientsActions.Location = new Point(ControlsU.X_FromCenter(btnClientsActions, Width / 2), UpY);
+            //btnClientsActions.ImageSize = new Size(ImageSize, ImageSize);
+
+            btnClientsTransfers.Size = size;
+            btnClientsTransfers.Location = new Point(Separator.Right - size.Width, UpY);
+
+            btnManageUsers.Size = size;
+            btnManageUsers.Location = new Point(Separator.Left, LowY);
+
+            btnUsersActions.Size = size;
+            btnUsersActions.Location = new Point(ControlsU.X_FromCenter(btnUsersActions, Width / 2), LowY);
+
+            btnLoginRegistry.Size = size;
+            btnLoginRegistry.Location = new Point(Separator.Right - size.Width, LowY);
+        }
+
+        private void btnManageUsers_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLoginRegistry_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void guna2CircleButton5_Click(object sender, EventArgs e)
+        private void btnUsersAction_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        private void btnManageClients_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClientsActions_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClientsTransfers_Click(object sender, EventArgs e)
         {
 
         }
